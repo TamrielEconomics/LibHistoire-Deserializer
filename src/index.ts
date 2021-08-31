4c9215a2a943896a32bb38e9e75a9f1787437474
NicolasNewman <nnewman316@gmail.com>
2021-08-31 20:20:22 +0000

    added ItemLink exportables to index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import { Deserialize } from './GuildHistoryCacheEntry';
+import ItemLink from './ItemLink/ItemLink';
+import Item from './ItemLink/Item';
+import Potion from './ItemLink/Potion';
+import { Level, LinkType, SubTypeMap, Quality, VR } from './ItemLink/types';
+import Writ from './ItemLink/Writ';
 
-export { Deserialize };
+export default Deserialize;
+export { ItemLink, Item, Potion, Writ, Level, LinkType, SubTypeMap, Quality, VR };
